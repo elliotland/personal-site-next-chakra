3aa4d7b0045fc6899273474f4cc220499679cbf0
elliotland <intellectspeaks@aim.com>
2024-07-20 22:06:05 -0400

    init

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,11 +7,12 @@ export const Hero = ({ title }: { title: string }) => (
     height="100vh"
     bgGradient="linear(to-l, heroGradientStart, heroGradientEnd)"
     bgClip="text"
+    
   >
     <Heading fontSize="6vw">{title}</Heading>
   </Flex>
 );
 
 Hero.defaultProps = {
-  title: "with-chakra-ui-typescript",
+  title: "Elliot Land",
 };
