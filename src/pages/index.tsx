491dbf13390a8188926afef8270f0dc57dfcb2e8
elliotland <intellectspeaks@aim.com>
2024-07-24 23:34:52 -0400

    button placement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,16 +29,44 @@ const Index = () => (
         color: "white",
       }}
       transition="all 0.15s ease-out"
+      display={"flex"}
+      flexDirection={"column"}
+      alignItems={"center"}
+      height={"100vh"}
     >
       <Hero title="Elliot Land" />
 
-      <Container centerContent marginTop={"4em"}>
-        <Button size={"lg"} className="animate-bounce ">
-          <ChevronDownIcon boxSize={"10"} />
-        </Button>
-      </Container>
+      <Container centerContent marginTop={"4em"}></Container>
 
       <DarkModeSwitch />
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"space-between"}
+         height={"100vh"}
+      >
+        <Text display={'flex'}>test</Text>
+        <Button
+          w={"10%"}
+          padding={"2em 2em 3em 2em"}
+          maxWidth={"50%"}
+          minWidth={"fit-content"}
+          size={"lg"}
+          className="animate-bounce"
+          display={"flex"}
+          flexDirection={"column"}
+          _light={{
+            bgColor: "blue",
+            color: "white",
+            _hover: {
+              bgColor: "blue.400",
+            },
+          }}
+        >
+          <ChevronDownIcon boxSize={"10"} />
+          Learn More
+        </Button>
+      </Box>
     </Box>
   </>
 );
