a1ee5e29a06b6b53c24b2122e130242aa867d789
elliotland <intellectspeaks@aim.com>
2024-07-25 23:08:55 -0400

    updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,16 @@ import {
   Container,
   Button,
   Center,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  Heading,
 } from "@chakra-ui/react";
 
 import { Hero } from "../components/Hero";
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
 import { ChevronDownIcon } from "@chakra-ui/icons";
+import { ClassNames } from "@emotion/react";
 
 const Index = () => (
   <>
@@ -36,37 +41,38 @@ const Index = () => (
     >
       <Hero title="Elliot Land" />
 
-      <Container centerContent marginTop={"4em"}></Container>
-
       <DarkModeSwitch />
-      <Box
+      <Container
         display={"flex"}
         flexDirection={"column"}
         justifyContent={"space-between"}
          height={"100vh"}
+         maxW={'container.lg'}
+         mt={"2em"}
       >
-        <Text display={'flex'}>test</Text>
+        
+        <Card w={'100%'} h={'50vh'} border={'1px solid tomato'}>
+        <CardHeader pos={'absolute'} top={'0'} left={'0'}><Heading>TL;DR</Heading></CardHeader>
+          <CardBody>
+            <Text>Some text</Text>
+          </CardBody>
+        </Card>
         <Button
           w={"10%"}
-          padding={"2em 2em 3em 2em"}
+          padding={"2em 2em 2em 2em"}
           maxWidth={"50%"}
           minWidth={"fit-content"}
           size={"lg"}
-          className="animate-bounce"
+          className={"animate-bounce"}
           display={"flex"}
           flexDirection={"column"}
-          _light={{
-            bgColor: "blue",
-            color: "white",
-            _hover: {
-              bgColor: "blue.400",
-            },
-          }}
+          alignSelf={'center'}
+          colorScheme={'blue'}
         >
           <ChevronDownIcon boxSize={"10"} />
-          Learn More
         </Button>
-      </Box>
+
+      </Container>
     </Box>
   </>
 );
